eb4ba4d526ec91cad2fc3cfc18de4e1707fa6e11
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-12-27 12:39:00 +0300

    Added buttons

--- start.py
+++ start.py
@@ -1,15 +1,48 @@
 import sqlite3
 import telebot
 import config
+from telebot import types
+import random
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands = ['start'])
 def welcome(message):
-    bot.send_message(message.chat.id, """Приветствую, {0.first_name}!\n Это бот, созданный для организации процесса стриминга в нашем проекте. Здесь ты можешь отмечать время, когда ты будешь стримить, а так же выполнять некоторые другие действия.""".format(message.from_user), parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    item1 = types.KeyboardButton("Начать!")
+    markup.add(item1)
+    bot.send_message(message.chat.id, """Приветствую, <b>{0.first_name}</b>!\n Это бот, созданный для организации процесса стриминга в нашем проекте. 
+    Здесь ты можешь отмечать время, когда ты будешь стримить, а так же выполнять некоторые другие действия.""".format(message.from_user), parse_mode='html', reply_markup=markup)
+
+    
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-    bot.send_message(message.chat.id, message.text) 
+    if message.chat.type == 'private':
+        if message.text == 'Начать!':
+            markup = types.InlineKeyboardMarkup(row_width=3)
+            item1 = types.InlineKeyboardButton("Добавить время", callback_data='new')
+            item2 = types.InlineKeyboardButton("Удалить время", callback_data='del')
+            item3 = types.InlineKeyboardButton("Статистика", callback_data='stat')
+            markup.add(item1, item2, item3)
+            bot.send_message(message.chat.id, 'Выбери, что ты хочешь сделать:', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Не понимаю тебя😢')
+ 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'new':
+                bot.send_message(call.message.chat.id, 'Добавляю в базу')
+            elif call.data == 'del':
+                bot.send_message(call.message.chat.id, 'Удаляю из базы')
+            elif call.data == 'stat':
+                bot.send_message(call.message.chat.id, 'Статистика:')
+
+ 
+ 
+    except Exception as e:
+        print(repr(e)) 
 
 bot.polling(none_stop = True)
