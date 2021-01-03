7774a43be6437bc94a785675becc753b730f54eb
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2021-01-03 23:53:45 +0300

    конфиг доделал + англ версия конфига

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ import re
 from Structure import Structure
 import json
 
-conf = 'CONFIG.json'
+conf = 'CONFIG_ENG.json'
 with open(conf) as jf:
     configuration = json.load(jf)
 bot = telebot.TeleBot(config.TOKEN)
@@ -36,10 +36,10 @@ def check_date(message):
     if re.match(re.compile(configuration['commands']["new"]['re_date_match']), message.text):
         bot.send_message(message.chat.id, configuration['commands']['new']['check_ok'])
         structure.date = message.text
-        bot.send_message(message.chat.id,configuration['commands']['new']['enter_time'],parse_mode='html')
+        bot.send_message(message.chat.id,configuration['commands']['new']['enter_time_beg'],parse_mode='html')
         bot.register_next_step_handler(message, check_time_new)
     else:
-        bot.send_message(message.chat.id,'Неверный формат даты, введите заново.')
+        bot.send_message(message.chat.id,configuration['commands']['new']['wrong_date'])
         bot.register_next_step_handler(message, check_date)
 
 
@@ -47,61 +47,61 @@ def check_time_new(message):
     if re.match(r'^(([0,1][0-9])|(2[0-3])):[0-5][0-9]$', message.text):
         bot.send_message(message.chat.id, 'Ок!')
         structure.time_begin = message.text
-        bot.send_message(message.chat.id,'Введите время, когда закончите стримить в формате: <b>12:25</b>', parse_mode='html')
+        bot.send_message(message.chat.id,configuration['commands']['new']['enter_time_end'], parse_mode='html')
         bot.register_next_step_handler(message, check_time_end)
     else:
-        bot.send_message(message.chat.id,'Неверный формат времени, введите заново.')
+        bot.send_message(message.chat.id,configuration['commands']['new']['wrong_time'])
         bot.register_next_step_handler(message, check_time_new)
 
 
 def check_time_end(message):
-    if re.match(r'^(([0,1][0-9])|(2[0-3])):[0-5][0-9]$', message.text):
-        bot.send_message(message.chat.id, 'Ок!')
+    if re.match(re.compile(configuration['commands']["new"]['re_time_match']), message.text):
+        bot.send_message(message.chat.id, configuration['commands']["new"]["check_ok"])
         structure.time_end = message.text
         print(message.from_user.first_name, structure.date,structure.time_begin, structure.time_end)
         db.add(message.from_user.first_name, structure.date,structure.time_begin, structure.time_end)
-        bot.send_message(message.chat.id, 'Записано!')
+        bot.send_message(message.chat.id, configuration['commands']["new"]["written"])
     else:
-        bot.send_message(message.chat.id,'Неверный формат времени, введите заново.')
+        bot.send_message(message.chat.id,configuration['commands']['new']['wrong_time'])
         bot.register_next_step_handler(message, check_time_end)
 
 
-@bot.message_handler(commands = [configuration['commands']['delete']])
+@bot.message_handler(commands = [configuration['commands']['delete']['name']])
 def delete(message):
-    bot.send_message(message.chat.id,'Список ваших записей:')
+    bot.send_message(message.chat.id,configuration['common']['all_user_data'])
     ls = db.get_users_streams(message.from_user.first_name)
     tmp = []
     for i in ls:
         tmp.append((i[1], i[2]))
     if len(tmp)==0:
-        bot.send_message(message.chat.id, 'У вас нет активных записей')
+        bot.send_message(message.chat.id, configuration['common']['no_data'])
         return
     res = ""
     res+='Date             Time\n'
     for i in tmp:
         res+=i[0]+' '+i[1]+'\n'
     bot.send_message(message.chat.id, res)
-    bot.send_message(message.chat.id,'Введите дату в формате и время в формате <b>2021/11/25 12:25</b> записи, которую вы хотите удалить',parse_mode='html')
+    bot.send_message(message.chat.id,configuration['commands']['delete']['enter_time_delete'],parse_mode='html')
     bot.register_next_step_handler(message, lambda msg: checking_delete(tmp, msg))
 
 def checking_delete(tmp, message):
-    if re.match(r'^202\d\/((1[0-2])|(0?[1-9]))\/((1[0-9])|(2[0-9])|(3[0-1])|(0?[0-9]))\s(([0,1][0-9])|(2[0-3])):[0-5][0-9]$', message.text):
-        db.delete(message.from_user.first_name,message.text.split()[0], message.text.split()[1])
-        bot.send_message(message.chat.id, 'Запись удалена!')
+    if re.match(re.compile(configuration['commands']['delete']['re_date_time_match']), message.text):
+        db.delete(message.from_user.first_name,message.text.split('-')[0], message.text.split('-')[1])
+        bot.send_message(message.chat.id, configuration['commands']['delete']['deleted'])
     else:
-        bot.send_message(message.chat.id,'Неверный формат , введите заново.')
+        bot.send_message(message.chat.id,configuration['commands']['delete']['wrong_format'])
         bot.register_next_step_handler(message, lambda msg: checking_delete(tmp, msg))
 
 
-@bot.message_handler(commands = [configuration['commands']['streams']])
+@bot.message_handler(commands = [configuration['commands']['streams']['name']])
 def get_streams(message):
-    bot.send_message(message.chat.id,'Список ваших записей:')
+    bot.send_message(message.chat.id,configuration['common']['all_user_data'])
     ls = db.get_users_streams(message.from_user.first_name)
     tmp = []
     for i in ls:
         tmp.append((i[1], i[2]))
     if len(tmp)==0:
-        bot.send_message(message.chat.id, 'У вас нет активных записей')
+        bot.send_message(message.chat.id, configuration['common']['no_data'])
         return
     res = ""
     res+='Date             Time\n'
@@ -109,16 +109,16 @@ def get_streams(message):
         res+=i[0]+' '+i[1]+'\n'
     bot.send_message(message.chat.id, res)
 
-@bot.message_handler(commands = [configuration['commands']['today']])
+@bot.message_handler(commands = [configuration['commands']['today']['name']])
 def today(message):
-    bot.send_message(message.chat.id,'Ваши стримы за сегодня:')
+    bot.send_message(message.chat.id,configuration['commands']['today']['streams_for_today'])
     today = datetime.date.today().strftime('%Y/%m/%d')
     ls = db.get_today_streams(today, message.from_user.first_name)
     tmp = []
     for i in ls:
         tmp.append((i[2], i[3]))
     if len(tmp)==0:
-        bot.send_message(message.chat.id, 'У вас сегодня нет стримов!')
+        bot.send_message(message.chat.id, configuration['commands']['today']['no_streams_for_today'])
         return
     res = ""
     res+='Begin      End\n'
